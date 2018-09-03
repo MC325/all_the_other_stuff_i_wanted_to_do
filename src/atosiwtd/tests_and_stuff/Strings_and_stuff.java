2740ea4724e40b90465b1dbd6bf3e390a0077df8
mc325 <mc325@MC-PC>
2018-09-02 21:53:07 -0600

    Added this random thing that is for me to test out ideas.

---
+++ src/atosiwtd/tests_and_stuff/Strings_and_stuff.java
@@ -0,0 +1,11 @@
+package atosiwtd.tests_and_stuff;
+
+import java.util.Scanner;
+
+public class Strings_and_stuff {
+	public static void main (String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		
+	}
+}
